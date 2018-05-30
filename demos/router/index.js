a70b5e39274919a66544a1e03fcfd1df1df2131b
xiaoluoboding <xiaoluoboding@gmail.com>
2018-05-30 16:52:08 +0800

    Update demos

--- demos/router/index.js
+++ demos/router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomeIndex from '@/views/index'
 import SmartGroup from '@/views/SmartGroup'
-import ShopifyDraggable from '@/components/ShopifyDraggable'
-import DraggableGrid from '@/components/DraggableGrid'
 
 Vue.use(Router)
 
@@ -22,16 +20,6 @@ export default new Router({
       path: '/smart-group',
       name: 'SmartGroup',
       component: SmartGroup
-    },
-    {
-      path: '/shopify-draggable',
-      name: 'shopify-draggable',
-      component: ShopifyDraggable
-    },
-    {
-      path: '/draggable-grid',
-      name: 'draggable-grid',
-      component: DraggableGrid
     }
   ]
 })
